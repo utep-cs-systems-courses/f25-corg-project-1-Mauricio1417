d15bb6cf80ed3dd9a57e99145654946c5c9d6720
Mauricio Mayorga <memayorga@miners.utep.edu>
2025-09-28 19:01:27 -0600

    Finished UI, fixed history.c

--- src/main.c
+++ src/main.c
@@ -4,30 +4,40 @@
 #include "history.h"
 
 int main() {
-  /*
+
+  // Linked List
+  List *list = init_history();
+  
   while (1) {
     printf("> ");
-
+    
     // User input
     char *str = malloc(100);
-    str[99] = '\0';
-    fgets(str, sizeof(str), stdin);
+    fgets(str, 100, stdin);
 
+    if (*str != '!') add_history(list, str);
+    
     // Tokenize string
     char **tokens = tokenize(str);
+
+    // User enters a command
+    if ((tokens[0] != NULL && tokens[1] == NULL) && *tokens[0] == '!') {
+      char *x = tokens[0];
+      x++;
+      if (*x == 'h') {
+	print_history(list);
+	continue;
+      }
+      char *p = tokens[0];
+      p++;
+      int id = atoi(p);
+
+      char *historyString = get_history(list, id);
+      char **tokensArr = tokenize(historyString);
+      print_tokens(tokensArr);
+      continue;
+    }
+    
     print_tokens(tokens);
-   
   }
-  */
-
-   printf("> ");
-
-    // User input
-    char *str = malloc(100);
-    str[99] = '\0';
-    fgets(str, sizeof(str), stdin);
-
-    // Tokenize string
-    char **tokens = tokenize(str);
-    print_tokens(tokens);
 }
